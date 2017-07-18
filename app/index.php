c693ad0f3bd767aeb1dfbf9f593dfb95d249d586
Andrea Cossio <andreacossio96@outlook.com>
2017-07-18 21:51:40 +0200

    .

--- app/index.php
+++ app/index.php
@@ -49,7 +49,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="../#home"><img src="photos/logo_white.png" alt="" class="img-responsive"></a>
+          <a class="navbar-brand" href="../#home"><img src="../photos/logo_white.png" alt="" class="img-responsive"></a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
