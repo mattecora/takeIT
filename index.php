e7e84ac41648b72f87fa87a3e1b059a26b87f399
mattecora <m96.corain@gmail.com>
2017-07-19 17:42:38 +0200

    Tag issue

--- index.php
+++ index.php
@@ -259,7 +259,7 @@
           <ul>
             <li class="active"><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
-            li><a href="#subscribe">Subscribe</a></li>
+            <li><a href="#subscribe">Subscribe</a></li>
             <li><a href="#faq">FAQ</a></li>
             <li><a href="login.php">Login</a></li>
           </ul>
