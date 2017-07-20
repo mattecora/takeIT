c5427d9ce6f39a13877efcb5048deff6d2b3291a
Andrea Cossio <andreacossio96@outlook.com>
2017-07-20 17:46:34 +0200

    Some footer links adjusted

--- app/functions.php
+++ app/functions.php
@@ -1,6 +1,6 @@
 <?php
   function db_connect() {
-    $db = new mysqli("localhost", "root", "", "my_eiatakeit");
+    $db = new mysqli("localhost", "eiatakeit", "", "my_eiatakeit");
     return $db;
   }
 
--- app/search.php
+++ app/search.php
@@ -145,11 +145,11 @@
 
       <div class="footer-links">
         <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#subscribe">Subscribe</a></li>
-          <li><a href="#faq">FAQ</a></li>
-          <li><a href="login.php">Login</a></li>
+          <li><a href="../">Home</a></li>
+          <li><a href="../#about">About</a></li>
+          <li><a href="../#subscribe">Subscribe</a></li>
+          <li><a href="../#faq">FAQ</a></li>
+          <li><a href="../login.php">Login</a></li>
         </ul>
       </div>
 
